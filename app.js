708e6c0cdc17009dbdc2885c51e68d5519c5addc
HayatoKTYM <hayakap1211@gmail.com>
2023-08-21 02:45:03 +0900

    add ejs: テンプレートファイルを追加

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
+app.set('view engine', 'ejs');
 const port = process.env.PORT || 3000;
 
 // ルートエンドポイント
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  // res.sendFile(path.join(__dirname + '/index.html'));
+  res.render('index');
 });
 
 // /health エンドポイント
@@ -15,6 +17,10 @@ app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
 
+app.get('/greet', (req, res) => {
+  const query = req.query.q || 'Express';
+  res.render('greet', { query: query });
+})
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
