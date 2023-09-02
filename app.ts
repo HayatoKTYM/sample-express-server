4133a75789311406351135301fb45aec93df852d
HayatoKTYM <hayakap1211@gmail.com>
2023-09-03 02:37:27 +0900

    add zod validation

--- app.ts
+++ app.ts
@@ -11,12 +11,16 @@ app.set('view engine', 'ejs');
 const port = process.env.PORT || 3000;
 
 const querySchema = z.object({
-  q: z.string().optional()
+  q: z.string(),
+  num: z.string().refine(
+    str => !isNaN(parseFloat(str)),
+    "Must be a valid number"
+  ).transform(str => parseFloat(str))
 });
 
 // ルートエンドポイント
 app.get('/', (req: Request, res: Response) => {
-  res.render('layout', { title: 'HOME', content: 'index' });
+  res.render('layout', { title: 'HOME', content: 'index'});
 });
 
 // /health エンドポイント
@@ -25,9 +29,13 @@ app.get('/health', (req: Request, res: Response) => {
 });
 
 app.get('/greet', (req: Request, res: Response) => {
+  console.log(req.query);
   const parsedQuery = querySchema.safeParse(req.query);
   const query = parsedQuery.success ? parsedQuery.data.q : 'Express';
-  res.render('layout', { title: 'Greet', content: 'greet', query: query });
+  const num = parsedQuery.success ? parsedQuery.data.num : 1;
+
+  // const parseNumberQuery = numberSchema.safeParse(req.)
+  res.render('layout', { title: 'Greet', content: 'greet', query: query.repeat(num) });
 });
 
 app.listen(port, () => {
